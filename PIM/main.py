d365913fcfe915e15a6e4fc9c6bef7b05f76a681
Zoezhouu <113340480+Zoezhouu@users.noreply.github.com>
2023-11-13 13:48:29 +0800

    Update main.py

--- PIM/main.py
+++ PIM/main.py
@@ -1,10 +1,17 @@
 class Note:
     def __init__(self,content):
         self.content = content
+    def getContent(self):
+        return self.content
+    def setContent(self, content):
+        self.content = content
+
+
     def display(self):
         print(self.content)
     def detail_display(self):
         print("The content for this note is {}".format(self.content))
+
 
 # class deadline:
 #     def __init__(self, d_date, d_time):
@@ -18,6 +25,14 @@ class Task:
     def __init__(self, description, deadline):
         self.description = description
         self.deadline = deadline
+    def getDescription(self):
+        return self.description
+    def setDescription(self, description):
+        self.description = description
+    def getDeadline(self):
+        return self.deadline
+    def setDeadline(self, deadline):
+        self.deadline = deadline
     def display(self):
         print(self.description)
         print(self.deadline)
@@ -29,6 +44,18 @@ class Event:
     def __init__(self, description, start_time, alarm):
         self.description = description
         self.start_time = start_time
+        self.alarm = alarm
+    def getDescription(self):
+        return self.description
+    def setDescription(self, description):
+        self.description = description
+    def getStartTime(self):
+        return self.start_time
+    def setStartTime(self, start_time):
+        self.start_time = start_time
+    def getAlarm(self):
+        return self.alarm
+    def setAlarm(self, alarm):
         self.alarm = alarm
     def display(self):
         print(self.description)
@@ -44,10 +71,26 @@ class Contact:
         self.name = name
         self.address = address
         self.mobile_num = mobile_num
+    def getName(self):
+        return self.name
+    def setName(self, name):
+        self.name = name
+    def getAddress(self):
+        return self.address
+    def setAddress(self, address):
+        self.address = address
+    def getMobileNum(self):
+        return self.mobile_num
+    def setMobileNum(self, mobile_num):
+        self.mobile_num = mobile_num
     def display(self):
         print(self.name)
         print(self.address)
         print(self.mobile_num)
+    def detail_display(self):
+        print("The name is {}".format(self.name))
+        print("His/Her address is {}". format(self.address))
+        print("His/Her mobile number is {}".format(self.mobile_num))
         
 
 def createNote(note_to_insert):
