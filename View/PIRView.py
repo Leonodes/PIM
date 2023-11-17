ac4ff98652b6da0ee32639d0a9c4e3a0dfd367c9
Zoe ZHOU <113340480+Zoezhouu@users.noreply.github.com>
2023-11-17 16:42:39 +0800

    Update PIRView.py

--- View/PIRView.py
+++ View/PIRView.py
@@ -1,19 +1,19 @@
-def printNoteDetail(noteContent):
+def NoteDetail(noteContent):
     print("Note: ")
     print("Content: "+ noteContent)
 
-def printTaskDetail(taskDesc, taskDeadline):
+def TaskDetail(taskDesc, taskDeadline):
     print("Task: ")
     print("Description: " + taskDesc)
     print("Deadline: " + taskDeadline)
 
-def printEventDetail(eventDesc, eventStart, eventAlarm):
+def EventDetail(eventDesc, eventStart, eventAlarm):
     print("Event:")
     print("Description: " + eventDesc)
     print("Start time: " + eventStart)
     print("Alarm: " + eventAlarm)
 
-def printContactDetail(contactName,contactAddr, contactNo):
+def ContactDetail(contactName,contactAddr, contactNo):
     print("Contact: ")
     print("Name: " + contactName)
     print("Address: " + contactAddr)
