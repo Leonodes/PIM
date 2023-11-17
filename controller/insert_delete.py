cf089f5a084a73e8cb3e3d90fc00cc23b5b55c03
Zoe ZHOU <113340480+Zoezhouu@users.noreply.github.com>
2023-11-17 15:39:01 +0800

    insert_delete.py

--- controller/insert_delete.py
+++ controller/insert_delete.py
@@ -21,6 +21,17 @@ def insert_line(csv_file, line, index):
         writer = csv.writer(file)
         writer.writerows(rows)
 
+def delete_line(csv_file, index):
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
+        rows = list(reader)
+
+    del rows[index]
+    
+    with open(csv_file, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(rows)
+
 def copy_csv_to_pim(pim_file, csv_file):
     with open(csv_file, 'r') as file:
         reader = csv.reader(file)
@@ -29,17 +40,19 @@ def copy_csv_to_pim(pim_file, csv_file):
     with open(pim_file, 'w') as file:
         file.write('\n'.join(lines))
 
+def insert(new_line,insert_index):
+    pim_file_path = 'records.pim'
+    csv_file_path = 'output.csv'
+    copy_pim_to_csv(pim_file_path,csv_file_path)
+    insert_line(csv_file_path,new_line,insert_index)
+    copy_csv_to_pim(pim_file_path,csv_file_path)
+    return
 
 
-
-
-
-# Test example
-pim_file_path = 'records.pim'
-csv_file_path = 'output.csv'
-new_line = ['Assignment 3', '2023/11/29 23:59']
-insert_index = 11
-
-copy_pim_to_csv(pim_file_path, csv_file_path)
-insert_line(csv_file_path, new_line, insert_index)
-copy_csv_to_pim(pim_file_path, csv_file_path)
+def delete(delete_index):
+    pim_file_path = 'records.pim'
+    csv_file_path = 'output.csv'
+    copy_pim_to_csv(pim_file_path,csv_file_path)
+    delete_line(csv_file_path, delete_index)
+    copy_csv_to_pim(pim_file_path,csv_file_path)
+    return
