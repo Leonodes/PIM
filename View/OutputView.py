84df0cb9d76524e21c885a23949ae9407a63d24e
Zoe ZHOU <113340480+Zoezhouu@users.noreply.github.com>
2023-11-19 17:28:09 +0800

    Update OutputView.py

--- View/OutputView.py
+++ View/OutputView.py
@@ -1,6 +1,36 @@
 class Board:
+    # instruction hints
+    def mainBoard():
+        print("Hi! Here is Personal Information Manager. Please choose what you want to do.")
+        print("1. Create Personal Information Records.")
+        print("2. Search Personal Information Records.")
+        print("3. Modify Personal Information Records.")
+        print("4. Delete Personal Information Records.")
+        print("5. Display Personal Information Records.")
+        print("6. Exit the system")
+    
     def createBoard():
         print("1. Create Quick Notes")
         print("2. Create tasks.")
         print("3. Create contacts.")
         print("4. Create events.")
+        print("5. Back to the Home Page")
+    
+    def searchBoard():
+        print("1. Search with text.")
+        print("2. Search with time.")
+        print("3. Search with logic.")
+        print("4. Back to the Home Page")
+
+    def modifyBoard():
+        #first search for PIR, then modify(use replace function)
+        pass
+
+    def deleteBoard():
+        #first search for PIR, then delete(use delete function)
+        pass
+
+    def displayBoard():
+        # displayall
+        # display single PIR
+        pass
