de1b546cf07b6590c1e9c97d27ebf1fcbefeffd3
Zoe ZHOU <113340480+Zoezhouu@users.noreply.github.com>
2023-11-19 15:26:00 +0800

    Update PIRNote.py

--- model/PIRNote.py
+++ model/PIRNote.py
@@ -3,8 +3,8 @@ class Note:
         self.content = content
     def getContent(self):
         return self.content
-    def setContent(self, content):
-        self.content = content
+    # def setContent(self, content):
+    #     self.content = content
     # def display(self):
     #     print(self.content)
     # def detail_display(self):
