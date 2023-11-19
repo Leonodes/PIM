eb1ff959c6e9c519ec3626d48acc329fdeef9a85
Zoe ZHOU <113340480+Zoezhouu@users.noreply.github.com>
2023-11-19 15:25:41 +0800

    Update PIRView.py

--- View/PIRView.py
+++ View/PIRView.py
@@ -1,25 +1,41 @@
+from model.PIRContact import Contact
+from model.PIREvent import Event
+from model.PIRNote import Note
+from model.PIRTask import Task
+
 class PIRView:
+    def __init__(self):
+        self.notes = Note
+        self.tasks = Task
+        self.events = Event
+        self.contacts = Contact
+
+    def displayAll(self):
+        
+        self.NoteDetail()
+
     @staticmethod
-    def NoteDetail(noteContent):
+    def NoteDetail(note):
+        # note = Note()
         print("Note: ")
-        print("Content: "+ noteContent)
+        print("Content: "+ note.content)
     
     @staticmethod
-    def TaskDetail(taskDesc, taskDeadline):
+    def TaskDetail(task):
         print("Task: ")
-        print("Description: " + taskDesc)
-        print("Deadline: " + taskDeadline)
+        print("Description: " + task.description)
+        print("Deadline: " + task.deadline)
 
     @staticmethod
-    def EventDetail(eventDesc, eventStart, eventAlarm):
+    def EventDetail(event):
         print("Event:")
-        print("Description: " + eventDesc)
-        print("Start time: " + eventStart)
-        print("Alarm: " + eventAlarm)
+        print("Description: " + event.description)
+        print("Start time: " + event.start_time)
+        print("Alarm: " + event.alarm)
 
     @staticmethod
-    def ContactDetail(contactName,contactAddr, contactNo):
+    def ContactDetail(contact):
         print("Contact: ")
-        print("Name: " + contactName)
-        print("Address: " + contactAddr)
-        print("Mobile Number" + contactNo)
+        print("Name: " + contact.name)
+        print("Address: " + contact.address)
+        print("Mobile Number" + contact.mobileNum)
