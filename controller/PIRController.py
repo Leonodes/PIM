2db43fea332969e5b4d3778f9d4bd92bc63fb4b3
Tammy0722 <tammy072203@gmail.com>
2023-11-20 20:10:01 +0800

    update model

--- controller/PIRController.py
+++ controller/PIRController.py
@@ -47,64 +47,64 @@ class PIRController:
         else:
             command = enter.createCommandAgain() # ask user to input again
 
-    # get note content
-    @staticmethod
-    def setPIMNote():
-        # get_note_content = input("Enter your note:")
-        # return get_note_content
-        enter = Command()
-        return enter.createNoteCommand()
+    # # get note content
+    # @staticmethod
+    # def setPIMNote():
+    #     # get_note_content = input("Enter your note:")
+    #     # return get_note_content
+    #     enter = Command()
+    #     return enter.createNoteCommand()
     
-    # get task content
-    @staticmethod
-    def setPIMTask():
-        # get_date = input("Enter date and time for task item ( in format MM/dd/yy hh:mm): ")
-        enter = Command()
-        get_date = enter.getDateCommand()
-        while not checkDate(get_date):
-            # print("Enter the right format date for task item:")
-            # get_date = input()
-            get_date = enter.getDateCommandAgain()
-        date = get_date
-        # taskItem = input("Enter task text:")
-        taskItem = enter.createTaskTextCommand()
-        return date, taskItem
+    # # get task content
+    # @staticmethod
+    # def setPIMTask():
+    #     # get_date = input("Enter date and time for task item ( in format MM/dd/yy hh:mm): ")
+    #     enter = Command()
+    #     get_date = enter.getDateCommand()
+    #     while not checkDate(get_date):
+    #         # print("Enter the right format date for task item:")
+    #         # get_date = input()
+    #         get_date = enter.getDateCommandAgain()
+    #     date = get_date
+    #     # taskItem = input("Enter task text:")
+    #     taskItem = enter.createTaskTextCommand()
+    #     return date, taskItem
     
-    # get contact content
-    @staticmethod
-    def setPIMContact():
-        enter = Command()
-        # get_name = input("Enter a name for contact item:")
-        get_name = enter.createContactNameCommand()
-        # get_addr = input("Enter an address for contact item: ")
-        get_addr = enter.createContactAddrCommand()
-        # get_mobileNum = int(input("Enter a number for contact item"))
-        get_mobileNum = enter.createContactMobileNumCommand()
+    # # get contact content
+    # @staticmethod
+    # def setPIMContact():
+    #     enter = Command()
+    #     # get_name = input("Enter a name for contact item:")
+    #     get_name = enter.createContactNameCommand()
+    #     # get_addr = input("Enter an address for contact item: ")
+    #     get_addr = enter.createContactAddrCommand()
+    #     # get_mobileNum = int(input("Enter a number for contact item"))
+    #     get_mobileNum = enter.createContactMobileNumCommand()
 
-        return get_name, get_addr, get_mobileNum
+    #     return get_name, get_addr, get_mobileNum
 
-    # get event content
-    @staticmethod
-    def setPIMEvent():
-        enter = Command()
-        # get_description = input("Enter a description for this event: ")
-        get_description = enter.createEventDescCommand()
+    # # get event content
+    # @staticmethod
+    # def setPIMEvent():
+    #     enter = Command()
+    #     # get_description = input("Enter a description for this event: ")
+    #     get_description = enter.createEventDescCommand()
         
-        # get_start_time = input("Enter start time for event item (MM/dd/yy hh:mm): ")
-        get_start_time = enter.getDateCommand()
-        while not checkDate(get_start_time):
-            # print("Enter the right format date for start time:")
-            # get_start_time = input()
-            get_start_time = enter.getDateCommandAgain()
+    #     # get_start_time = input("Enter start time for event item (MM/dd/yy hh:mm): ")
+    #     get_start_time = enter.getDateCommand()
+    #     while not checkDate(get_start_time):
+    #         # print("Enter the right format date for start time:")
+    #         # get_start_time = input()
+    #         get_start_time = enter.getDateCommandAgain()
         
-        # get_alarm = input("Enter a time to alarm you (MM/dd/yy hh:mm):")
-        get_alarm = enter.getDateCommand()
-        while not checkDate(get_alarm):
-            # print("Enter the right format date for alarm time:")
-            # get_alarm = input()
-            get_alarm = enter.getDateCommandAgain()
+    #     # get_alarm = input("Enter a time to alarm you (MM/dd/yy hh:mm):")
+    #     get_alarm = enter.getDateCommand()
+    #     while not checkDate(get_alarm):
+    #         # print("Enter the right format date for alarm time:")
+    #         # get_alarm = input()
+    #         get_alarm = enter.getDateCommandAgain()
 
-        return get_description, get_start_time, get_alarm
+    #     return get_description, get_start_time, get_alarm
     
     # Display PIRs
 
